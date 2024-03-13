7816786ecf0f1c70cd7bc3ad4d796bbc9af2fac9
jcbhmr <jcbhmr@outlook.com>
2024-03-12 22:10:07 -0500

    add shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import * as path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.BASE_PATH,
   plugins: [react()],
+  // https://ui.shadcn.com/docs/installation/vite#update-viteconfigts
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
