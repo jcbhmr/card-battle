bb49273c84e31df1e533344abe832a556a12681a
browntj16 <157514480+browntj16@users.noreply.github.com>
2024-04-02 14:16:30 -0500

    Created card component
    Created component named CardComponent. Only works for creatures for now.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,67 @@
 import { useState } from "react";
 import placeholderSVGURL from "./assets/placeholder.svg"
+import { Game } from "./model";
+
+
+
+/**
+ * v0 by Vercel.
+ * @see https://v0.dev/t/WuNN16G0fnd
+ * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
+ * 
+ * TODO: rework this to make it work for spells/buildings
+ * 
+ * @returns Returns a card component
+ * @param cardName: the actual name of the card, cardText: the description of the text on the card,
+ * actionCost: the cost of the card, landscapeType: the type of landscape that the card needs, 
+ * attack and defense: the offensive and defensive stats of the card respectively, imagePath: the 
+ * location of the image for the card
+ */
+function CardComponent({cardName, cardText, actionCost, landscapeType,
+attack, defense, imagePath}: { cardName: string, cardText: string, actionCost: number, 
+  landscapeType: string, attack: number, defense: number, imagePath: string}) {
+  return (
+      <div className="flex flex-col rounded-lg border overflow-hidden w-[200px]">
+        <div className="flex aspect-16/9">
+          <img
+            alt={cardName}
+            className="object-cover"
+            height={135}
+            src={imagePath}
+            style={{
+              aspectRatio: "240/135",
+              objectFit: "cover",
+            }}
+            width={350}
+          />
+        </div>
+        <div className="flex-1 p-4 grid gap-2">
+          <h2 className="text-lg font-bold tracking-tight">{cardName}</h2>
+          <p className="text-sm line-clamp-3">{cardText}</p>
+          <div>
+            <div>Action Cost: {actionCost}</div>
+            <div>Landscape Type: {landscapeType}</div>
+            <div>Attack: {attack}</div>
+            <div>Defense: {defense}</div>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+//not finished
+// function handOfCards(){
+//   let playerHand = tempGameObject.players[0].hand
+//   let shownHand = []
+//   for(let i = 0; i < playerHand.length; i++){
+    
+//     shownHand.push(CardComponent(playerHand[i].name, playerHand[i].flavorText, playerHand[i].cost, 
+//       playerHand[i].landscapeType, playerHand[i]))
+//   }
+//   return(
+//     <CardComponent></CardComponent>
+//   )
+// }
 
 function AppBoard() {
   return (
