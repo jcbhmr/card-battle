f61e6df6c4361d82b1a2e964f05da98a299c9c90
browntj16 <browntj16@uww.edu>
2024-04-03 19:56:07 -0500

    Added function that generates a hand of cards

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ import { Game } from "./model";
  * @see https://v0.dev/t/WuNN16G0fnd
  * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
  * 
- * TODO: rework this to make it work for spells/buildings
  * 
  * @returns Returns a card component
  * @param cardName: the actual name of the card, cardText: the description of the text on the card,
@@ -17,7 +16,7 @@ import { Game } from "./model";
  * attack and defense: the offensive and defensive stats of the card respectively, imagePath: the 
  * location of the image for the card
  */
-function CardComponent({cardName, cardText, actionCost, landscapeType,
+function CreatureComponent({cardName, cardText, actionCost, landscapeType,
 attack, defense, imagePath}: { cardName: string, cardText: string, actionCost: number, 
   landscapeType: string, attack: number, defense: number, imagePath: string}) {
   return (
@@ -49,19 +48,74 @@ attack, defense, imagePath}: { cardName: string, cardText: string, actionCost: n
   )
 }
 
-//not finished
-// function handOfCards(){
-//   let playerHand = tempGameObject.players[0].hand
-//   let shownHand = []
-//   for(let i = 0; i < playerHand.length; i++){
-    
-//     shownHand.push(CardComponent(playerHand[i].name, playerHand[i].flavorText, playerHand[i].cost, 
-//       playerHand[i].landscapeType, playerHand[i]))
-//   }
-//   return(
-//     <CardComponent></CardComponent>
-//   )
-// }
+/**
+ * v0 by Vercel.
+ * @see https://v0.dev/t/WuNN16G0fnd
+ * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
+ * 
+ * 
+ * @returns Returns a card component
+ * @param cardName: the actual name of the card, cardText: the description of the text on the card,
+ * actionCost: the cost of the card, landscapeType: the type of landscape that the card needs,
+ * imagePath: the location of the image for the card
+ */
+function CardComponent({cardName, cardText, actionCost, landscapeType, imagePath}: 
+  { cardName: string, cardText: string, actionCost: number,landscapeType: string,imagePath: string}) {
+    return (
+        <div className="flex flex-col rounded-lg border overflow-hidden w-[200px]">
+          <div className="flex aspect-16/9">
+            <img
+              alt={cardName}
+              className="object-cover"
+              height={135}
+              src={imagePath}
+              style={{
+                aspectRatio: "240/135",
+                objectFit: "cover",
+              }}
+              width={350}
+            />
+          </div>
+          <div className="flex-1 p-4 grid gap-2">
+            <h2 className="text-lg font-bold tracking-tight">{cardName}</h2>
+            <p className="text-sm line-clamp-3">{cardText}</p>
+            <div>
+              <div>Action Cost: {actionCost}</div>
+              <div>Landscape Type: {landscapeType}</div>
+            </div>
+          </div>
+        </div>
+    )
+  }
+
+
+/**
+ * This method will (currently) only make an array of "generic" card/creature components. This method WILL
+ * have to be updated to get a players hand from 
+ * @returns 
+ */
+function HandOfCards(){
+  // let playerHand = tempGameObject.players[0].hand
+  let shownHand = []
+  //for(let i = 0; i < playerHand.length; i++){
+  for(let i = 0; i < 5; i++){
+    // let card = playerHand[i];
+    //if(card.constructor.name == "Creature"){
+    if(true){
+      // shownHand.push(CreatureComponent({cardName: card.name, cardText:card.flavorText, actionCost: card.cost,
+      // landscapeType: card.landscapeType, attack: card.attack,defense:card.defense,imagePath: ""}))
+      shownHand.push(CreatureComponent({cardName: "name", cardText: "flavorText", actionCost: 0,
+      landscapeType: "landscapeType", attack: 0, defense:0,imagePath: ""}))
+    }
+    else{
+      shownHand.push(CardComponent({cardName:"name", cardText: "text", actionCost:0, landscapeType: "landscape",
+    imagePath: ""}))
+    }
+  }
+  return(
+    shownHand
+  )
+}
 
 function AppBoard() {
   return (
@@ -154,14 +208,19 @@ function AppBoard() {
   )
 }
 
-
+function Test({myString}: {myString: string}){
+  return(
+      <p>{myString}</p>
+  )
+}
 
 function App() {
   return (
     <div className="flex justify-center items-center h-screen p-4">
-      <AppBoard />
+      <AppBoard/>
     </div>
   );
 }
 
 export default App;
+
