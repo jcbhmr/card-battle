57a0a3f6326f7cd8cf20212777e648de2e0ab570
Jacob Hummer <jcbhmr@outlook.com>
2024-05-07 19:35:17 +0000

    save

--- src/engine/CardMap.ts
+++ src/engine/CardMap.ts
@@ -48,14 +48,14 @@ class Deck {
 //Made this card more for testing or ending the game quickly if we have to.
 put(
   "Swamp Dragon",
-  new Creature("Swamp Dragon", "", 2, LandscapeType.Swamp, 25, 8, "src/assets/swamp-dragon.png"),
+  new Creature("Swamp Dragon", "", 2, LandscapeType.Swamp, 25, 8, ""),
 );
 put(
   "Dark Angel",
-  new Creature("Dark Angel", "", 1, LandscapeType.Swamp, 10, 5, "src/assets/dark-angel.png"),
+  new Creature("Dark Angel", "", 1, LandscapeType.Swamp, 10, 5, ""),
 );
 
-put("Bog Bum", new Creature("Bog Bum", "", 1, LandscapeType.Swamp, 2, 6, "src/assets/bog-bum.png"));
+put("Bog Bum", new Creature("Bog Bum", "", 1, LandscapeType.Swamp, 2, 6, ""));
 
 // Not sure about this one. This card's ability is not really functional right now.
 put(
@@ -67,7 +67,7 @@ put(
     LandscapeType.Swamp,
     1,
     6,
-    "src/assets/flyswatter.png",
+    "",
   ),
 );
 
@@ -979,3 +979,115 @@ function getCornfieldDeck() {
     ],
   );
 }
+
+
+import n0 from "../assets/swamp-dragon.png"; addUrl("Swamp Dragon", n0)
+import n1 from "../assets/dark-angel.png"; addUrl("Dark Angel", n1)
+import n2 from "../assets/bog-bum.png"; addUrl("Bog Bum", n2)
+import n3 from "../assets/fly-swatter.png"; addUrl("Fly Swatter", n3)
+import n4 from "../assets/unicylops.png"; addUrl("Unicylops", n4)
+import n5 from "../assets/man-witch.png"; addUrl("Man Witch", n5)
+import n6 from "../assets/ban-she-princess.png"; addUrl("Ban-She Princess", n6)
+import n7 from "../assets/ban-she-queen.png"; addUrl("Ban-She Queen", n7)
+import n8 from "../assets/ban-she-knight.png"; addUrl("Ban-She Knight", n8)
+import n9 from "../assets/black-merlock.png"; addUrl("Black Merlock", n9)
+import n10 from "../assets/bog-ban-she-angel.png"; addUrl("Bog Ban-She Angel", n10)
+import n11 from "../assets/eye-dude.png"; addUrl("Eye Dude", n11)
+
+import n12 from "../assets/giant-spider.png"; addUrl("Giant Spider", n12)
+
+import n13 from "../assets/burning-eyebat.png"; addUrl("Burning Eyebat", n13)
+
+import n14 from "../assets/instigator.png"; addUrl("InstiGator", n14)
+
+import n15 from "../assets/axe-stump.png"; addUrl("Axe Stump", n15)
+
+import n16 from "../assets/skeleton-hand.png"; addUrl("Skeleton Hand", n16)
+
+import n17 from "../assets/undying-tree.png"; addUrl("Undying Tree", n17)
+
+import n18 from "../assets/bald-guy.png"; addUrl("Bald Guy", n18)
+
+import n19 from "../assets/the-sludge.png"; addUrl("The Sludge", n19)
+
+import n20 from "../assets/sludgebob.png"; addUrl("Sludgebob", n20)
+
+import n21 from "../assets/general-mushroom.png"; addUrl("General Mushroom", n21)
+
+import n22 from "../assets/swamp-beast.png"; addUrl("Swamp Beast", n22)
+
+import n23 from "../assets/flying-gator.png"; addUrl("Flying Gator", n23)
+
+import n24 from "../assets/orange-slime-monster.png"; addUrl("Orange Slime Monster", n24)
+
+import n25 from "../assets/green-mermaid.png"; addUrl("Green Mermaid", n25)
+
+import n26 from "../assets/green-merman.png"; addUrl("Green Merman", n26)
+
+import n27 from "../assets/furious-hen.png"; addUrl("Furious Hen", n27)
+
+import n28 from "../assets/furious-rooster.png"; addUrl("Furious Rooster", n28)
+
+import n29 from "../assets/papercut-tiger.png"; addUrl("Papercut Tiger", n29)
+
+import n30 from "../assets/cottonpult.png"; addUrl("Cottonpult", n30)
+
+import n31 from "../assets/music-mallard.png"; addUrl("Music Mallard", n31)
+
+import n32 from "../assets/beach-mum.png"; addUrl("Beach Mum", n32)
+
+import n33 from "../assets/black-cat.png"; addUrl("Black Cat", n33)
+
+import n34 from "../assets/burning-hand.png"; addUrl("Burning Hand", n34)
+
+import n35 from "../assets/cactus-thug.png"; addUrl("Cactus Thug", n35)
+
+import n36 from "../assets/count-cactus.png"; addUrl("Count Cactus", n36)
+
+import n37 from "../assets/fummy.png"; addUrl("Fummy", n37)
+
+import n38 from "../assets/giant-mummy-hand.png"; addUrl("Giant Mummy Hand", n38)
+
+import n39 from "../assets/green-cactaball.png"; addUrl("Green Cactaball", n39)
+
+import n40 from "../assets/green-party-ogre.png"; addUrl("Green Party Ogre", n40)
+
+import n41 from "../assets/lady-mary.png"; addUrl("Lady Mary", n41)
+
+import n42 from "../assets/lime-slimey.png"; addUrl("Lime Slimey", n42)
+
+import n43 from "../assets/lost-golem.png"; addUrl("Lost Golem", n43)
+
+import n44 from "../assets/mayonaise-angel.png"; addUrl("Mayonaise Angel", n44)
+
+import n45 from "../assets/ms-mummy.png"; addUrl("Ms. Mummy", n45)
+
+import n46 from "../assets/mud-angel.png"; addUrl("Mud Angel", n46)
+
+import n47 from "../assets/pieclops.png"; addUrl("Pieclops", n47)
+
+import n48 from "../assets/prickle.png"; addUrl("Prickle", n48)
+
+import n49 from "../assets/sandbacho.png"; addUrl("Sandbacho", n49)
+
+import n50 from "../assets/sand-angel.png"; addUrl("Sand Angel", n50)
+
+import n51 from "../assets/sand-eyebat.png"; addUrl("Sand Eyebat", n51)
+
+import n52 from "../assets/sand-jackal.png"; addUrl("Sand Jackal", n52)
+
+import n53 from "../assets/sand-knight.png"; addUrl("Sand Knight", n53)
+
+import n54 from "../assets/sandasaurus-rex.png"; addUrl("Sandasaurus Rex", n54)
+
+import n55 from "../assets/sandfoot.png"; addUrl("Sandfoot", n55)
+
+import n56 from "../assets/sandsnake.png"; addUrl("Sandsnake", n56)
+
+import n57 from "../assets/sandwitch.png"; addUrl("Sandwitch", n57)
+
+import n58 from "../assets/sandy.png"; addUrl("Sandy", n58)
+
+import n59 from "../assets/wall-of-chocolate.png"; addUrl("Wall of Chocolate", n59)
+
+import n60 from "../assets/wall-of-sand.png"; addUrl("Wall of Sand", n60)
